d5f55e5a6bee406b7a6efcfe601dc428ec425f27
qiudeng7 <2143975743@qq.com>
2026-02-14 14:18:24 +0800

    分离k8s和kind

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 # 创建集群并部署应用
 kind delete clusters selfhost && \ 
-kind create cluster --config manifests/kind.yaml --name selfhost && \
-kubectl apply -k manifests/
+kind create cluster --config kind/config.yaml --name selfhost && \
+kubectl apply -k k8s/
 
 # todo
 # 部署集群外反向代理
